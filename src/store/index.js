30fe5c49a38b44e33369563a6a930144a9ab7266
Therese Persen <therese.persen@me.com>
2020-08-18 15:07:55 +0200

    add logger to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,19 @@
 import { createStore, combineReducers } from 'redux';
 import servicesReducer from 'reducers';
 
+const addLoggerToDispatch = (store) => {
+  const dispatch = store.dispatch;
+
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = dispatch(action);
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
 const initStore = () => {
   const app = combineReducers({
     service: servicesReducer,
@@ -11,6 +24,7 @@ const initStore = () => {
     window.__REDUX_DEVTOOLS_EXTENSION__();
 
   const store = createStore(app, browserSupport);
+  store.dispatch = addLoggerToDispatch(store);
   return store;
 };
 
