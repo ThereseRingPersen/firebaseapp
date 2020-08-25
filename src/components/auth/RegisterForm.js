6b01efa86445aea027c8373b233e37b7fce05f53
Therese Persen <therese.persen@me.com>
2020-08-25 10:58:18 +0200

    add form validation

--- src/components/auth/RegisterForm.js
+++ src/components/auth/RegisterForm.js
@@ -1,116 +1,144 @@
-import React from 'react';
+/* eslint no-useless-escape: 0 */
 
-const RegisterForm=()=>{
-    return (
-      <div className='auth-page'>
-        <div className='container has-text-centered'>
-          <div className='column is-4 is-offset-4'>
-            <h3 className='title has-text-grey'>Register</h3>
-            <p className='subtitle has-text-grey'>
-              Please Register to proceed.
-            </p>
-            <div className='box'>
-              <figure className='avatar'>
-                <img src='https://placehold.it/128x128' />
-              </figure>
-              <form>
-                <div className='field'>
-                  <div className='control'>
-                    <input
-                      name='email'
-                      className='input is-large'
-                      type='email'
-                      placeholder='Your Email'
-                      autofocus=''
-                      autocomplete='email'
-                    />
-                    <div className='form-error'>
-                      <span className='help is-danger'>Email is required</span>
-                      <span className='help is-danger'>
-                        Email address is not valid
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <div className='field'>
-                  <div className='control'>
-                    <input
-                      name='fullName'
-                      className='input is-large'
-                      type='text'
-                      placeholder='Full Name'
-                      autofocus=''
-                    />
-                    <div className='form-error'>
-                      <span className='help is-danger'>Name is required</span>
-                      <span className='help is-danger'>Name is not valid</span>
-                    </div>
-                  </div>
-                </div>
-                <div className='field'>
-                  <div className='control'>
-                    <input
-                      name='avatar'
-                      className='input is-large'
-                      type='text'
-                      placeholder='Avatar'
-                      autofocus=''
-                    />
-                    <div className='form-error'>
-                      <span className='help is-danger'>Avatar is required</span>
-                      <span className='help is-danger'>
-                        Avatart is not valid
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <div className='field'>
-                  <div className='control'>
-                    <input
-                      name='password'
-                      className='input is-large'
-                      type='password'
-                      placeholder='Your Password'
-                      autocomplete='current-password'
-                    />
-                    <div className='form-error'>
-                      <span className='help is-danger'>
-                        Password is required
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <div className='field'>
-                  <div className='control'>
-                    <input
-                      name='passwordConfirmation'
-                      className='input is-large'
-                      type='password'
-                      placeholder='Repeat Password'
-                      autocomplete='current-password'
-                    />
-                    <div className='form-error'>
-                      <span className='help is-danger'>
-                        Password Confirmation is required
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <button
-                  type='submit'
-                  className='button is-block is-info is-large is-fullwidth'
-                >
-                  Register
-                </button>
-              </form>
+import React from 'react';
+import useForm from 'react-hook-form';
+
+import { formValidators } from './utils';
+
+const RegisterForm = () => {
+  const { register, handleSubmit, errors } = useForm();
+
+  const getFormData = (data) => {
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(getFormData)}>
+      <div className='field'>
+        <div className='control'>
+          <input
+            ref={register(formValidators.email)}
+            name='email'
+            className='input is-large'
+            type='email'
+            placeholder='Your Email'
+            autoComplete='email'
+          />
+          {errors.email && (
+            <div className='form-error'>
+              {errors.email.type === 'required' && (
+                <span className='help is-danger'>Email is required</span>
+              )}
+              {errors.email.type === 'pattern' && (
+                <span className='help is-danger'>
+                  Email address is not valid
+                </span>
+              )}
             </div>
-            <p className='has-text-grey'>
-              <a>Sign In With Google</a>&nbsp;
-              <a href='/'>Sign Up</a> &nbsp;·&nbsp;
-              <a href='../'>Need Help?</a>
-            </p>
-          </div>
+          )}
         </div>
       </div>
-    );
-}
+      <div className='field'>
+        <div className='control'>
+          <input
+            ref={register(formValidators.fullName)}
+            name='fullName'
+            className='input is-large'
+            type='text'
+            placeholder='Full Name'
+          />
+          {errors.fullName && (
+            <div className='form-error'>
+              {errors.fullName.type === 'required' && (
+                <span className='help is-danger'>Name is required</span>
+              )}
+              {errors.fullName.type === 'minLength' && (
+                <span className='help is-danger'>
+                  Minimum length is 10 characters
+                </span>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+      <div className='field'>
+        <div className='control'>
+          <input
+            ref={register(formValidators.avatar)}
+            name='avatar'
+            className='input is-large'
+            type='text'
+            placeholder='Avatar'
+          />
+          {errors.avatar && (
+            <div className='form-error'>
+              {errors.avatar.type === 'required' && (
+                <span className='help is-danger'>Avatar is required</span>
+              )}
+              {errors.avatar.type === 'pattern' && (
+                <span className='help is-danger'>Avatar url is not valid</span>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+      <div className='field'>
+        <div className='control'>
+          <input
+            ref={register(formValidators.passWord)}
+            name='password'
+            className='input is-large'
+            type='password'
+            placeholder='Your Password'
+            autoComplete='current-password'
+          />
+          {errors.password && (
+            <div className='form-error'>
+              {errors.password.type === 'required' && (
+                <span className='help is-danger'>Password is required</span>
+              )}
+              {errors.password.type === 'minLength' && (
+                <span className='help is-danger'>
+                  Minimum length is 6 characters
+                </span>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+      <div className='field'>
+        <div className='control'>
+          <input
+            ref={register(formValidators.passWord)}
+            name='passwordConfirmation'
+            className='input is-large'
+            type='password'
+            placeholder='Repeat Password'
+            autoComplete='current-password'
+          />
+          {errors.passwordConfirmation && (
+            <div className='form-error'>
+              {errors.passwordConfirmation.type === 'required' && (
+                <span className='help is-danger'>
+                  Password confirmation is required
+                </span>
+              )}
+              {errors.passwordConfirmation.type === 'minLength' && (
+                <span className='help is-danger'>
+                  Minimum length is 6 characters
+                </span>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+      <button
+        type='submit'
+        className='button is-block is-info is-large is-fullwidth'
+      >
+        Register
+      </button>
+    </form>
+  );
+};
+
+export default RegisterForm;
