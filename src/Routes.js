d8c8977e38585c390d5f5054da80ae29e933ac65
Therese Persen <therese.persen@me.com>
2020-08-25 14:02:49 +0200

    pull routes out in separate container

---
+++ src/Routes.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Faq from './pages/Faq';
+import Profile from './pages/Profile';
+import Services from './pages/Services';
+import ServiceDetails from './pages/ServiceDetail';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path='/services/:serviceId'>
+        <ServiceDetails />
+      </Route>
+      <Route path='/services'>
+        <Services />
+      </Route>
+      <Route path='/login'>
+        <Login />
+      </Route>
+      <Route path='/register'>
+        <Register />
+      </Route>
+      <Route path='/profile'>
+        <Profile />
+      </Route>
+      <Route path='/faq'>
+        <Faq />
+      </Route>
+      <Route path='/'>
+        <Home />
+      </Route>
+    </Switch>
+  );
+};
+export default Routes;
