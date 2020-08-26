a46200f119247662fba2d71ab83af91f7b563927
Therese Persen <therese.persen@me.com>
2020-08-26 11:02:15 +0200

    refactor api, separating into separate files

---
+++ src/api/auth.js
@@ -0,0 +1,50 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import db from 'db';
+
+export async function register({ email, password, fullName, avatar }) {
+  try {
+    const res = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password);
+    const { user } = res;
+    const userProfile = {
+      uid: user.uid,
+      fullName,
+      email,
+      avatar,
+      services: [],
+      description: '',
+    };
+    await createUserProfile(userProfile);
+    return userProfile;
+  } catch (error) {
+    return Promise.reject(error.message);
+  }
+}
+
+export function logout(){
+    firebase.auth().signOut();
+} 
+export function login({ email, password }) {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch((error) => Promise.reject(error.message));
+}
+
+export function onAuthStateChanged (onAuthCallback){
+     firebase.auth().onAuthStateChanged(onAuthCallback);
+}
+ 
+
+export function createUserProfile(userProfile) {
+  db.collection('profile').doc(userProfile.uid).set(userProfile);
+}
+export function getUserProfile(uid){
+    db.collection('profiles')
+      .doc(uid)
+      .get()
+      .then((snapshot) => ({ uid, ...snapshot.data() }));
+}
--- src/api/index.js
+++ src/api/index.js
@@ -1,54 +1,8 @@
 import db from 'db';
 
-import firebase from 'firebase/app';
-import 'firebase/auth';
+export const createRef = (collection, docId) =>
+  db.doc(`${collection}/` + docId);
 
-export const createUserProfile = (userProfile) => {
-  db.collection('profile').doc(userProfile.uid).set(userProfile);
-};
-
-export const fetchServiceById = (serviceId) =>
-  db
-    .collection('services')
-    .doc(serviceId)
-    .get()
-    .then((snapshot) => ({ id: snapshot.id, ...snapshot.data() }));
-
-export const fetchServices = () =>
-  db
-    .collection('services')
-    .get()
-    .then((snapshot) => {
-      const services = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      return services;
-    });
-
-export const register = async ({ email, password, fullName, avatar }) => {
-  try {
-    const res = await firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password);
-    const { user } = res;
-    const userProfile = {
-      uid: user.uid,
-      fullName,
-      email,
-      avatar,
-      services: [],
-      description: '',
-    };
-    await createUserProfile(userProfile);
-    return userProfile;
-  } catch (error) {
-    return Promise.reject(error.message);
-  }
-};
-
-export const login = ({ email, password }) =>
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .catch((error) => Promise.reject(error.message));
+export * from './services';
+export * from './auth';
+export * from './connection';
