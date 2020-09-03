935ad086111545dc0f6dcb6861bfd5ae4a0a9851
Therese Persen <therese.persen@me.com>
2020-09-03 13:14:35 +0200

    refactor from class to functional component

--- src/ServiceApp.js
+++ src/ServiceApp.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Sidebar from 'components/Sidebar';
 import Navbar from 'components/Navbar';
+import Spinner from 'components/Spinner';
 import Routes from './Routes';
+import { logout } from 'actions';
 
-class ServiceApp extends React.Component {
-  renderApplication = () => (
-    <React.Fragment>
-      <Navbar />
-      <Navbar id='navbar-clone' />
-      <Sidebar />
-      <Routes />
-    </React.Fragment>
-  );
+const ServiceApp = () => {
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(logout());
+  };
 
-  render() {
-    return this.renderApplication();
+  const auth = useSelector((state) => state.auth);
+
+  return renderApplication(auth);
+
+  function renderApplication(auth) {
+    return (
+      <React.Fragment>
+        <Navbar auth={auth} logout={handleLogout} />
+        <Navbar auth={auth} logout={handleLogout} id='navbar-clone' />
+        <Sidebar />
+        <Routes />
+      </React.Fragment>
+    );
   }
-}
+};
 
 export default ServiceApp;
