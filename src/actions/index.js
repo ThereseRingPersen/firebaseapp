b9c38f4b4457689922eda5efa878287dacfb6cfe
Therese Persen <therese.persen@me.com>
2020-09-03 13:09:10 +0200

    part of renameing file

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,8 @@
 export { fetchServices, fetchServiceById } from 'actions/services';
 export { register } from 'actions/register';
 export { loginUser } from 'actions/login';
-export { onAuthStateChanged, storeAuthUser } from 'actions/authorization';
+export {
+  onAuthStateChanged,
+  storeAuthUser,
+  logout,
+} from 'actions/authorization';
